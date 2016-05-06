457b69bcc0cf8ea4080c8b03e60a08b4e6a4f613
KCostanzo <kstanz22@gmail.com>
2016-05-06 16:03:47 -0700

    fix prod file

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,11 @@
 var webpack = require('webpack');
 
-var path = require("path");
-
 module.exports = {
   context: __dirname,
   entry: "./frontend/cloudsound.jsx",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js",
-    devtoolModuleFilenameTemplate: '[resourcePath]',
-    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+    path: "./app/assets/javascripts",
+    filename: "bundle.js"
   },
   plugins:[
     new webpack.DefinePlugin({
