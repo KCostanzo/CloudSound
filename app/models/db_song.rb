070a8296bca6d2b2beeceeb70ada56b200b939e7
KCostanzo <kstanz22@gmail.com>
2017-03-28 11:40:09 -0700

    default audio broken

--- app/models/db_song.rb
+++ app/models/db_song.rb
@@ -1,9 +1,8 @@
 class DbSong < ActiveRecord::Base
 	validates :user_id, presence: true
 	
-	has_attached_file :aws_song, default_url: "Nas_Damian_Jr_Gong_Marley_-_Nah_Mean[www.MP3Fiber.com].mp3"
+	has_attached_file :aws_song, default_url: "../assets/Nah_Mean.mp3"
 	# validates_attachment_content_type :aws_song, content_type: /\Aaudio\/.*\z/
-
 
 	belongs_to :user
 end
