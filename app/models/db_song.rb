9d83ff125945ae911e800ebda47d662bfa9862e1
KCostanzo <kstanz22@gmail.com>
2017-05-03 15:42:29 -0700

    readme chagne

--- app/models/db_song.rb
+++ app/models/db_song.rb
@@ -1,7 +1,7 @@
 class DbSong < ActiveRecord::Base
 	validates :user_id, presence: true
 	
-	has_attached_file :aws_song, default_url: "../assets/Nah_Mean.mp3"
+	has_attached_file :aws_song, default_url: "Nah_Mean.mp3"
 	# validates_attachment_content_type :aws_song, content_type: /\Aaudio\/.*\z/
 
 	belongs_to :user
