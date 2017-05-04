570c4b20f9d3d15a55681624015ee02ad90fc00f
KCostanzo <kstanz22@gmail.com>
2017-05-04 15:56:53 -0700

    refactoring

--- frontend/cloudsound.jsx
+++ frontend/cloudsound.jsx
@@ -6,10 +6,7 @@ var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 var IndexRoute = require('react-router').IndexRoute;
 var hashHistory = require('react-router').hashHistory;
-// var Store = require('./stores/session_store.js');
-// var SongStor = require('./stores/song_store.js');
-// var PlayStore = require('./stores/play_store.js');
-// var UStore = require('./stores/user_store.js');
+
 var Likes = require('./stores/likes_store.js');
 var ClientActions = require('./actions/client_actions.js');
 
