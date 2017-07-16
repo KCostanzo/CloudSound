0bb03d7795d8d8aa3df9fec628f84468487625a1
KCostanzo <kstanz22@gmail.com>
2017-07-16 13:48:02 -0700

    clean db app files out if not needed

--- app/models/db_song.rb
+++
@@ -1,8 +0,0 @@
-class DbSong < ActiveRecord::Base
-	validates :user_id, presence: true
-	
-	has_attached_file :aws_song, default_url: "Nah_Mean.mp3"
-	# validates_attachment_content_type :aws_song, content_type: /\Aaudio\/.*\z/
-
-	belongs_to :user
-end
--- app/views/db_songs/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @db_song, :id, :created_at, :updated_at
