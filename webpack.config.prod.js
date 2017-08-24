6e74496bc2b7b13c12f09b309512eb3ac6288ab0
KCostanzo <kstanz22@gmail.com>
2017-08-24 14:51:49 -0700

    checking config files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 var webpack = require("webpack");
 
+
 module.exports = {
   context: __dirname,
   entry: "./frontend/cloudsound.jsx",
@@ -7,32 +8,60 @@ module.exports = {
     path: "./app/assets/javascripts",
     filename: "bundle.js"
   },
-  plugins:[
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
-      }
-    })
-  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['react']
+          presets: ["react"]
         }
       }
     ]
   },
-  devtool: 'source-map',
+  devtool: 'source-maps',
   resolve: {
-    extensions: ["", ".js", ".jsx"]
+    extensions: ["", ".js", ".jsx" ]
   }
 };
+
+
+
+
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/cloudsound.jsx",
+//   output: {
+//     path: "./app/assets/javascripts",
+//     filename: "bundle.js"
+//   },
+//   plugins:[
+//     new webpack.DefinePlugin({
+//       'process.env':{
+//         'NODE_ENV': JSON.stringify('production')
+//       }
+//     }),
+//     new webpack.optimize.UglifyJsPlugin({
+//       compress:{
+//         warnings: true
+//       }
+//     })
+//   ],
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         loader: 'babel',
+//         query: {
+//           presets: ['react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: ["", ".js", ".jsx"]
+//   }
+// };
